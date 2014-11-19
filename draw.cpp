f96e5ae983f16291d732269dcb8d45fe3b047435
NikeKhin <nikmk@gmx.de>
2014-11-19 23:36:31 +0300

    добавлены фракталы в последний фрейм

--- draw.cpp
+++ draw.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <cmath>
+#include <random>
+#include <complex>
+#include <typeinfo>
 
 #include "gif.hpp"
 
@@ -7,48 +10,100 @@ using namespace std;
 
 const int size = 256;
 
+class Pixel
+{
+public:
+    void red(uint8_t color){
+        px &= 0xFFFFFF00;
+        px |= static_cast<uint32_t>(color);
+    }
+    void green(uint8_t color){
+        px &= 0xFFFF00FF;
+        px |= static_cast<uint32_t>(color) << 8;
+    }
+    void blue(uint8_t color){
+        px &= 0xFF00FFFF;
+        px |= static_cast<uint32_t>(color) << 16;
+    }
+    Pixel& operator=(uint32_t color){
+        px=color;
+        return *this;
+    }
+private:
+    uint32_t px{0};
+};
+
+
 class Frame
 {
 public:
-    uint32_t arr[size][size];
+    Pixel& point(int row, int col){return arr[row][col];}
+    operator void*(){return arr;}
+private:
+    Pixel arr[size][size];
 };
 
 int main()
 {
+    // Массив пикселей
     Frame frame;
 
     // создание объекта (gif-файла) с заданным именем и размерами
     GifWriter gw("checking.gif",size,size);
 
-    // диагональный градиент красным
+    // диагональный градиент
     for(int i=0;i<size;i++)
         for(int j=0;j<size;j++)
-            frame.arr[i][j]=uint8_t((i+j)/2);
-    gw.write(frame.arr);
+        {
+            frame.point(i,j).red( (i+j)/2 );    // заливка красным
+            frame.point(i,j).green( (i+j)/4 );  // добавляем немного зеленого
+            frame.point(i,j).blue( (i+j)/3 );   // и синего
+        }
+    gw.write(frame);
 
-    // горизонтальный градиент зеленым
+    // все однотонное
     for(int i=0;i<size;i++)
         for(int j=0;j<size;j++)
-            frame.arr[i][j]=uint8_t(j) << 8; //RGB - каждому 8 бит, поэтому для зеленого, идущего вторым, сдвиг на 8
-    gw.write(frame.arr);
-
-    // вертикальный градиент синим
-    for(int i=0;i<size;i++)
-        for(int j=0;j<size;j++)
-            frame.arr[i][j]=uint8_t(i) << 16; //RGB - для синего, идущего третьим, сдвиг на 16
-    gw.write(frame.arr);
-
-
-    // все белое
-    for(int i=0;i<size;i++)
-        for(int j=0;j<size;j++)
-            frame.arr[i][j]=-1;
-    gw.write(frame.arr);
+            frame.point(i,j)=0x00AAFFAA;
+    gw.write(frame);
 
     // синусоида
     for(int i=0;i<size;i++)
-        frame.arr[127+int(50*sin(0.1*i))][i]=0;
-    gw.write(frame.arr);
+        frame.point( size/2+int(size/4*sin(0.1*i)), i) = 0;
+    gw.write(frame);
+
+    // случайная заливка
+    std::default_random_engine generator;
+    std::normal_distribution<double> distribution(0.0, 0.5); // нормальное распределение со средним 127 и СКО 32
+    for(int i=0;i<size;i++)
+        for(int j=0;j<size;j++)
+        {
+            frame.point(i,j).red  ( distribution(generator) );    // заливка красным
+            frame.point(i,j).green( distribution(generator) );  // добавляем немного зеленого
+            frame.point(i,j).blue ( distribution(generator) );   // и синего
+        }
+    gw.write(frame);
+
+    complex<double> c={-0.2,0.7};
+    for(int i=0;i<size;i++)
+    {
+        for(int j=0;j<size;j++)
+        {
+            frame.point(i,j) = 0;
+            complex<double> x ={static_cast<double>(j)/size-0.5, static_cast<double>(i)/size-0.5};
+            x *= 3;
+            for(int n=0; n<20; n++)
+            {
+                x = x*x+c;
+                if(std::abs(x) > 10.0 )
+                {
+                    frame.point(i,j) = n*255/20;
+                    break;
+                }
+            }
+        }
+    }
+    gw.write(frame);
 
 
     return 0;
