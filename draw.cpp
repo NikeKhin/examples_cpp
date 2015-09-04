0bb93f2a70eaacd3c12789a4c9a55e20a3ae98c8
unknown <naa@anikekhin.ms.lan>
2015-09-04 19:21:24 +0300

    started more fractals

--- draw.cpp
+++ draw.cpp
@@ -65,9 +65,10 @@ public:
         x2 = xx2;
         y2 = yy2;
     }
-    void paint(Frame &f){
-        for(int y=y1; y<=y2; y++) // по строчкам
-            for(int x=x1; x<=x2; x++) // по столбцам
+    void paint(Frame &f)
+    {
+        for(int y=y1; y<=y2; y++) // rows
+            for(int x=x1; x<=x2; x++) // cols
             {
                 double w = x2-x1;
                 double h = y2-y1;
@@ -82,46 +83,33 @@ public:
 
 int main()
 {
-    // Массив пикселей
+    setlocale(LC_ALL, "");
+    //SetConsoleOutputCP(1251);
+    //SetConsoleCP(1251);
+
+
+    // Pixel 2D frame
     Frame frame;
-    for(int i=0; i<size; i++) // по строчкам
-        for(int j=0; j<size; j++) // по столбцам
-            frame[i][j] = 0;
 
-    // создание объекта (gif-файла) с заданным именем и размерами
-    GifWriter gw("checking.gif",size,size,250);
-
-    Shape t(20,20, 100, 60);
-    t.paint(frame);
-/*
-    for(int i=237; i<300; i++) // по строчкам
-        for(int j=237; j<300; j++) // по столбцам
-            frame[i][j].red(255), frame[i][j].blue(255);
-*/
-
-    gw.write(frame);
+    // Construct the animated GIF. The last parameter is animation delay
+    GifWriter gw("julia.gif", size, size, 250);
 
 
-
-/*
- многострочный комментарий
-*/
-/*
-    // Множество Жюлиа (http://elementy.ru/posters/fractals)
+    // Julia set (https://en.wikipedia.org/wiki/Julia_set, http://elementy.ru/posters/fractals)
     for(int cc=-10; cc<=10; cc++)
     {
-        cout << "current step=" << cc << endl;
+        wcout << L"current step (С€Р°Рі) =" << cc << endl;
         complex<double> c= {(-0.2 + cc*0.01 ),(0.7 - cc*0.01)};
-        for(int i=0; i<size; i++) // по строчкам
+        for(int i=0; i<size; i++) // rows
         {
-            for(int j=0; j<size; j++) // по столбцам
+            for(int j=0; j<size; j++) // columns
             {
                 frame[i][j] = 0;
                 complex<double> x = {static_cast<double>(j)/size-0.5, static_cast<double>(i)/size-0.5};
                 x *= 3;
                 for(int n=0; n<50; n++)
                 {
-                    x = x*x+c; // итерации x[k+1]=x[k]*x[k]+c;
+                    x = x*x+c; // iterate x[k+1]=x[k]*x[k]+c;
                     if(std::abs(x) > 10.0 )
                     {
                         frame[i][j].red(n*255/20);
@@ -132,9 +120,63 @@ int main()
                 }
             }
         }
-        //gw.write(frame);
+        gw.write(frame);
     }
-*/
+
+    //Newton fractal(https://en.wikipedia.org/wiki/Newton_fractal, http://elementy.ru/posters/fractals/Newton)
+    // РњРЅРѕР¶РµСЃС‚РІРѕ Р–СЋР»РёР° (http://elementy.ru/posters/fractals)
+    for(int cc=-10; cc<=10; cc++)
+    {
+        wcout << L"current step=" << cc << endl;
+        complex<double> c= {(1 + cc*0.01),(0 - cc*0.01)};
+        for(int i=0; i<size; i++) // РїРѕ СЃС‚СЂРѕС‡РєР°Рј
+        {
+            for(int j=0; j<size; j++) // РїРѕ СЃС‚РѕР»Р±С†Р°Рј
+            {
+                frame[i][j] = 0;
+                complex<double> x = {static_cast<double>(j)/size-0.5, static_cast<double>(i)/size-0.5};
+                complex<double> d1 = {size, 1};
+                complex<double> d2 = {size, size};
+                complex<double> d3 = {1, size};
+                complex<double> o = {1,0};
+                complex<double> a = {3,0};
+                complex<double> b = {2,0};
+                complex<double> x1 = {0, 0};
+                complex<double> res1 = {0,0};
+                double eps = 0.0000001;
+                //double eps2 = 0.0001;
+                //double eps3 = 0.1;
+
+
+                x *= 30;
+                for(int n=0; n<50; n++)
+                {
+                    x1 = x;
+                    x = c * (o+b*x*x*x)/(a*x*x);
+                    //res = (std::abs(x) - std::abs(x1)) / std::abs(x);
+                    if ((std::abs(x) - std::abs(x1)) > eps )
+                    {
+                        res1 = (d1-x)/x;
+                        frame[i][j].red(n*res1.real());
+                        res1 = (d2-x)/x;
+                        frame[i][j].green(n*res1.real());
+                        res1 = (d3-x)/x;
+                        frame[i][j].blue(n*res1.real());
+                        break;
+                    }
+                }
+            }
+        }
+        gw.write(frame);
+    }
+
+
+
+    //https://en.wikipedia.org/wiki/Mandelbrot_set
+    //https://en.wikipedia.org/wiki/Collatz_conjecture#Collatz_fractal
+    //https://en.wikibooks.org/wiki/Fractals/The_Newton-Raphson_fractal, http://elementy.ru/posters/fractals/Halley
+    //http://formulas.ultrafractal.com/
+
 
     return 0;
 }
